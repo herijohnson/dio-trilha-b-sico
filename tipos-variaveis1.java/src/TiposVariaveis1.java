7e28d837e0ea0e88459552bb7e1b752430742c52
herijohnson <herijohnsonhj7@gmail.com>
2023-01-13 14:17:44 -0300

    Atualizando Projeto

--- tipos-variaveis1.java/src/TiposVariaveis1.java
+++ tipos-variaveis1.java/src/TiposVariaveis1.java
@@ -1,5 +1,19 @@
-public class App {
+public class TiposVariaveis1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR DE PI = 3.14;
+
     }
 }
